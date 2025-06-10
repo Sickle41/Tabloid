9f3024b286901279a9a986cc65f6d99d72c5fcd3
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-10 09:46:17 -0500

    changed subscription model

--- Models/Subscription.cs
+++ Models/Subscription.cs
@@ -1,15 +1,22 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Tabloid.Models
 {
     public class Subscription
     {
         public int Id { get; set; }
+
         [Required]
         public int UserProfileId { get; set; }
+
         [Required]
         public int AuthorId { get; set; }
+
+        [ForeignKey("UserProfileId")]
         public UserProfile? UserProfile { get; set; }
+
+        [ForeignKey("AuthorId")]
         public UserProfile? AuthorProfile { get; set; }
     }
 }
