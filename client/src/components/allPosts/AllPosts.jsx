1f938b5ebb5bfa618fb549a80da4a3bcb025ca1b
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-11 15:02:49 -0500

    displaying info according to ticket

--- client/src/components/allPosts/AllPosts.jsx
+++ client/src/components/allPosts/AllPosts.jsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react"
+import { getAllPosts } from "../../managers/postManager"
+import "./AllPosts.css"
+
 export const AllPosts = () => {
-    return <h2>ALLPOSTS</h2>
+    const [allPosts, setAllPosts] = useState([])
+
+    useEffect(() => {
+        getAllPosts().then(setAllPosts)
+    }, [])
+
+    return (
+        <div>
+            <h2>All Posts</h2>
+            {allPosts.map(p => <div className="post-container" key={p.id}>
+                <h3>{p.title}</h3>
+                <span>{"Author: "} <h4>{p.userProfile?.firstName ?? "NA"}</h4></span>
+                <span>{"Category: "}<h4>{p.category?.name ?? "NA"}</h4></span>
+            </div>)}
+        </div>
+    )
 }
