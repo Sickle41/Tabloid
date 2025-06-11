55de3a87745c7b9c49f6f82f79332f4a252727cf
Joey Blackwell <blackwellj6241@gmail.com>
2025-06-11 12:00:37 -0500

    finished create form

--- client/src/managers/postManager.js
+++ client/src/managers/postManager.js
@@ -3,3 +3,13 @@ const _apiUrl = "/api/posts";
 export const getAllPosts = () => {
   return fetch(_apiUrl).then((res) => res.json());
 };
+
+export const createNewPost = (post) => {
+  return fetch(_apiUrl,{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(post),
+  }).then((res)=>res.json());
+};
