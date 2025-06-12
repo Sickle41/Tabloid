36c77b4f9151453777424310bc48d9c999ead07e
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-06-12 10:05:46 -0500

    post details wip

--- client/src/components/postDetails/PostDetails.jsx
+++ client/src/components/postDetails/PostDetails.jsx
@@ -1,3 +1,7 @@
-export const PostDetails = ({postId}) => {
+import { useParams } from "react-router-dom"
+
+export const PostDetails = () => {
+    const {postId} = useParams()
+    
     return <h2>Post {postId}</h2>
 }
